4c1de77986a027d7d1dae1e20385f5351ac513d3
Carsten Przyluczky <carsten.przyluczky@9elements.com>
2016-02-23 11:33:27 +0100

    removed code files

--- CHANGELOG.md
+++
@@ -1,5 +0,0 @@
-# Changelog
-All notable changes to this project will be documented in this file.
-The img.ly SDK adheres to [Semantic Versioning](http://semver.org/).
-
----
--- Tests/imglyKit-Tests-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  imglyKit-iOS-Tests-Bridging-Header.h
-//  imglyKit
-//
-//  Created by Sascha Schwabbauer on 04/01/16.
-//  Copyright © 2016 9elements GmbH. All rights reserved.
-//
-
-#import <imglyKit/LUTToNSDataConverter.h>
